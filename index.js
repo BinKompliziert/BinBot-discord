7d3426dce85082d26c395dadae90efc4e52c70f3
BinKompliziert <legolasnb@gmx.de>
2025-07-12 05:42:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -83,8 +83,8 @@ client.on('messageReactionRemove', async (reaction, user) => {
 })
 
 client.on('guildMemberAdd', (member) => {
-  member.roles.add('1339266733831229533')
-  member.roles.add('1393390109671292979')
+  member.guild.id == '1339144979435749398' ? member.roles.add('1339266733831229533') :
+  member.guild.id == '1393346424615211219' ? member.roles.add('1393390109671292979') : ''
 })
 
 client.on('messageCreate', async (message) => {
